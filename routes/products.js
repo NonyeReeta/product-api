40b5107be7520b9da6f7bf4c65dedeffea89df54
nonyereeta <nonyeibeanu@gmail.com>
2023-04-25 12:48:40 +0100

    added filter parameter to route

--- routes/products.js
+++ routes/products.js
@@ -2,10 +2,10 @@ const express = require("express");
 const productModel = require("../models/products");
 const productRoute = express.Router();
 
-productRoute.get('/products', async (req, res) => {
+productRoute.get('/approved-products', async (req, res) => {
     const { page } = req.query;
     productModel
-        .find()
+        .find({status: 'Approved'})
         .skip((page - 1) * 10)
         .limit(10)
         .then((products) => {
@@ -15,6 +15,20 @@ productRoute.get('/products', async (req, res) => {
             res.status(500).send(err.message)
         });
 })
+
+productRoute.get("/pending-products", async (req, res) => {
+  const { page } = req.query;
+  productModel
+    .find({ status: "Not Approved" })
+    .skip((page - 1) * 10)
+    .limit(10)
+    .then((products) => {
+      res.send(products);
+    })
+    .catch((err) => {
+      res.status(500).send(err.message);
+    });
+});
 
 productRoute.post('/add-product', (req, res) => {
     const productDetails = req.body;
